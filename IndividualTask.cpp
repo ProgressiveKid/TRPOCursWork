7024f002dd92c10fc5edb64953cdb179f1ae907f
Влад Голуб <neongrant3916@mail.ru>
2023-05-12 08:57:03 +0300

    резервная копия перед объявлением всех функции в заголовках

--- IndividualTask.cpp
+++ IndividualTask.cpp
@@ -6,6 +6,53 @@ void IndividualTask::Surt(string filename)
 
 }
 
+void ShowInformationOnTime(string choose)
+{
+	vector<CallRecord> records = ReadFromFileClass::getDataArray(GlobalVariablesClass::UsersFile);
+	if (choose == "time")
+	{
+
+
+
+
+
+
+	}
+	else if (choose == "date")
+	{
+
+
+
+
+
+	}
+}
+
+void ChooseTime() // 
+{
+	int choice = 0;
+	do {
+		std::cout << "Выберите параметр:\n";
+		std::cout << "1. Вывод информации по времени\n";
+		std::cout << "2. Вывод информации по дате\n";
+		std::cout << "3. Назад\n";
+		std::cin >> choice;
+		switch (choice) {
+		case 1:
+			ShowInformationOnTime("time");
+			break;
+		case 2:
+			ShowInformationOnTime("date");
+			break;
+		case 3:
+
+			break;
+		default:
+			std::cout << "Неправильный выбор.\n";
+			break;
+		}
+	} while (choice != 3);
+}
 
 void IndividualTask::IndividualTasksInterface()
 {
@@ -24,7 +71,7 @@ void IndividualTask::IndividualTasksInterface()
 
 		switch (choice) {
 		case 1:
-
+			
 			break;
 		case 2:
 			break;
@@ -44,4 +91,5 @@ void IndividualTask::IndividualTasksInterface()
 	} while (choice != 4);
 
 
-}
+};
+
